68cd9c971d60f9cb9a3bedb87bbd0c55d10dd05c
Ian Bailey <iankbailey2@gmail.com>
2019-10-30 16:16:24 -0400

    initial commit

---
+++ ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ColorGuessingGame
+//
+//  Created by Ian Bailey on 10/30/19.
+//  Copyright © 2019 Ian Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var color: UIView!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+       
+        
+        
+        
+    }
+
+
+}
+
+//let myColor = UIColor(red: 0.50,
+//                             green: 0.50,
+//                             blue: 0.50,
+//           alpha: 1)
