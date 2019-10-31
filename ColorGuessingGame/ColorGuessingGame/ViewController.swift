ee3ee0515191ef4a5759793281f16b1238d61759
Ian Bailey <iankbailey2@gmail.com>
2019-10-31 12:54:04 -0400

    Finished Project, Need to refactor

--- ColorGuessingGame/ColorGuessingGame/ViewController.swift
+++ ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -15,18 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var redButton: UIButton!
     @IBOutlet weak var greenButton: UIButton!
     @IBOutlet weak var blueButton: UIButton!
-    
+    @IBOutlet weak var currentScore: UILabel!
+    @IBOutlet weak var highScore: UILabel!
+    @IBOutlet weak var reset: UIButton!
     
     var arrayOfColorValues = [CGFloat]()
     var red = CGFloat.random(in: 0.0...1.0)
     var green = CGFloat.random(in: 0.0...1.0)
     var blue = CGFloat.random(in: 0.0...1.0)
     var score = 0
-    
+    var highscore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     arrayOfColorValues = [red,green,blue]
         updateColor()
     }
 
@@ -45,7 +46,18 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func resetButton(_ sender: UIButton) {
+        updateColor()
+        restart()
+        updateScore()
+        updateHighScore()
+    }
+    
     func updateColor() {
+        red = CGFloat.random(in: 0.0...1.0)
+        green = CGFloat.random(in: 0.0...1.0)
+        blue = CGFloat.random(in: 0.0...1.0)
+        arrayOfColorValues = [red, green, blue]
           color.backgroundColor = .init(red: arrayOfColorValues[0], green: arrayOfColorValues[1], blue: arrayOfColorValues[2], alpha: 1)
     }
     
@@ -56,15 +68,39 @@ class ViewController: UIViewController {
     
     func rightOrWrong(float: CGFloat, button: CGFloat) {
         if float == button {
-            print("That is correct")
             score += 1
-            
-            
+            arrayOfColorValues.removeAll()
+            updateColor()
+            updateHighScore()
+            updateScore()
+           
         } else {
-            print("buzz! Wrong")
+            gameOver()
         }
     }
     
-
+    func updateScore() {
+        currentScore.text = "Current Score: \(score)"
+    }
+    
+    func updateHighScore() {
+        if score > highscore {
+            highscore = score
+            highScore.text = "High Score: \(highscore)"
+        }
+    }
+    
+    func gameOver() {
+        redButton.isEnabled = false
+        greenButton.isEnabled = false
+        blueButton.isEnabled = false
+    }
+    
+    func restart() {
+        score = 0
+        redButton.isEnabled = true
+        greenButton.isEnabled = true
+        blueButton.isEnabled = true
+    }
 }
 
